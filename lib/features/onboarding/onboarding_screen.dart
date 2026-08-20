8d7633c1b53958e94f892c33a44b8393b7e48335
Malak Abualrob <m.abualrob24@student.aaup.edu>
2026-08-20 12:59:09 +0300

    section20,video7: Enhance onboarding screen with skip button and improved layout

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -11,31 +11,44 @@ class OnboardingScreen extends StatelessWidget {
         itemCount: OnboardingModel.onboardingList.length,
         itemBuilder: (BuildContext context, int index) { 
           final OnboardingModel model = OnboardingModel.onboardingList[index];
-          return  Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(model.image),
-              SizedBox(height: 24),
-              Text(
-                model.title,
-                style: TextStyle(
-                  fontSize: 20, 
-                  color: Color(0XFF4E4B66),
-                  fontWeight: FontWeight.w700,                
+          return  Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: (){},
+                    style: TextButton.styleFrom(
+                      foregroundColor: Color(0XFFC53030),
+                    ), 
+                    child: Text('Skip',style: TextStyle(fontSize: 16)),
+                    
+                    )
+                  ),
+                Image.asset(model.image),
+                SizedBox(height: 24),
+                Text(
+                  model.title,
+                  style: TextStyle(
+                    fontSize: 20, 
+                    color: Color(0XFF4E4B66),
+                    fontWeight: FontWeight.w700,                
+                  ),
                 ),
-              ),
-              SizedBox(height: 12),
-              Text(
-                model.description,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16, 
-                  color: Color(0XFF6E7191),
-                  fontWeight: FontWeight.w400,                
+                SizedBox(height: 12),
+                Text(
+                  model.description,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16, 
+                    color: Color(0XFF6E7191),
+                    fontWeight: FontWeight.w400,                
+                  ),
                 ),
-              ),
-                
-            ],
+                  
+              ],
+            ),
           );
         },
       ),
