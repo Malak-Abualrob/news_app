489dd8dda3c4df4358d3ba8e828934b3ec671260
Malak Abualrob <m.abualrob24@student.aaup.edu>
2026-08-22 22:59:13 +0300

    section21,video14:  implement home screen UI and controller for fetching news headlines

---
+++ lib/features/home/home_cotroller.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:news_app/core/datasource/remote_data/api_config.dart';
+import 'package:news_app/core/datasource/remote_data/api_service.dart';
+import 'package:news_app/features/home/models/news_article_model.dart';
+
+class HomeController extends ChangeNotifier {
+  bool topHeadlineLoading = true;
+  bool everthingLoading = true;
+  String? errorMessage;
+
+  List<NewsArticleModel> newsTopHeadLineList = [];
+  List<NewsArticleModel> newsEverythingList = [];
+  ApiService apiService = ApiService();
+
+  getTopHeadLine() async {
+    try {
+      Map<String, dynamic> result = await apiService.get(
+        ApiConfig.topHeadlines,
+        params: {"country": "us"},
+      );
+
+      newsTopHeadLineList = (result["articles"] as List)
+          .map((e) => NewsArticleModel.fromJson(e))
+          .toList();
+      topHeadlineLoading = false;
+      errorMessage = null;
+    } catch (e) {
+      topHeadlineLoading = false;
+      errorMessage = e.toString();
+    }
+    notifyListeners();
+  }
+
+  getEverythinge() async {
+    try {
+      Map<String, dynamic> result = await apiService.get(
+        ApiConfig.everything,
+        params: {"q": "news"},
+      );
+
+      newsEverythingList = (result["articles"] as List)
+          .map((e) => NewsArticleModel.fromJson(e))
+          .toList();
+      everthingLoading = false;
+      errorMessage = null;
+    } catch (e) {
+      everthingLoading = false;
+      errorMessage = e.toString();
+    }
+
+    notifyListeners();
+  }
+}
--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -5,7 +5,6 @@ import 'package:news_app/core/datasource/remote_data/api_config.dart';
 import 'package:news_app/core/datasource/remote_data/api_service.dart';
 import 'package:news_app/features/home/models/news_article_model.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -14,15 +13,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  List<NewsArticleModel> newsTopHeadLineList =[];
-  List<NewsArticleModel> newsEverythingList =[];
-  ApiService apiService = ApiService();
-  
-  bool isLoading= true;
-  String? errorMessage;
-
-
   @override
   void initState() {
     getTopHeadLine();
@@ -30,84 +20,25 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
   }
 
-  getTopHeadLine()async{
-    try{
-      setState(() {
-         isLoading= true;
-         errorMessage=null;
-      });
-    Map<String,dynamic> result = await apiService.get(
-      ApiConfig.topHeadlines,
-      params: {
-        "country":"us"
-      },
-      );
-
-    setState((){
-          newsTopHeadLineList = (result["articles"] as List)
-          .map((e) => NewsArticleModel.fromJson(e))
-          .toList();
-         isLoading= false;
-         errorMessage=null;
-    });
-
-    }catch(e){
-      setState(() {
-      isLoading= false;
-      errorMessage = e.toString();
-      });
-    }
-  }
-    getEverythinge()async{
-      try{
-        setState(() {
-         isLoading= true;
-         errorMessage=null;
-        });
-      Map<String,dynamic> result = await apiService.get(
-      ApiConfig.everything,
-      params: {
-        "q":"news"
-      },
-      );
-  
-    setState((){
-          newsEverythingList = (result["articles"] as List)
-          .map((e) => NewsArticleModel.fromJson(e))
-          .toList();
-         isLoading= false;
-         errorMessage=null;
-    });
-
-      }catch(e){
-      setState(() {
-      isLoading= false;
-      errorMessage = e.toString();
-      });
-     }
-
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-      (errorMessage?.isNotEmpty ?? false)
-      ?Center(child: Text(errorMessage!))
-      :isLoading
-      ?Center(child: CircularProgressIndicator()) :
-       Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: newsTopHeadLineList.length,
-              itemBuilder: (BuildContext context, int index) {
-              return Text(newsTopHeadLineList[index].title);
-            },
+      body: (errorMessage?.isNotEmpty ?? false)
+          ? Center(child: Text(errorMessage!))
+          : isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: newsTopHeadLineList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Text(newsTopHeadLineList[index].title);
+                    },
+                  ),
+                ),
+              ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
