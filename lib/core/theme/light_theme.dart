f6df611d386731d160937d8305a2506ede3c0b24
Malak Abualrob <m.abualrob24@student.aaup.edu>
2026-08-23 01:27:21 +0300

    section22,video2: implement light theme configuration and color palette constants

--- lib/core/theme/light_theme.dart
+++ lib/core/theme/light_theme.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/core/theme/light_color.dart';
 
-BottomNavigationBarThemeData bottomNavigationBarTheme =
-    BottomNavigationBarThemeData(
-      backgroundColor: Color(0XFFF5F5F5),
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: Color(0XFFC53030),
-      unselectedItemColor: Color(0XFF363636),
-      showUnselectedLabels: true,
-    );
+ThemeData themeData = ThemeData(
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: LightColor.primaryColor,
+      foregroundColor: Color(0xFFFFFCFC),
+      textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+    ),
+  ),
+
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(foregroundColor: LightColor.primaryColor),
+  ),
+
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    backgroundColor: LightColor.backgroundColor,
+    type: BottomNavigationBarType.fixed,
+    selectedItemColor: LightColor.primaryColor,
+    unselectedItemColor: Color(0xFF363636),
+    showUnselectedLabels: true,
+  ),
+);
